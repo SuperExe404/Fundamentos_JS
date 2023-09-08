007c29f2e9df275c80c364ef92e56fb7de5bde5a
SuperExe404 <diegomon0440@gmail.com>
2023-09-08 14:56:33 -0600

    Operadores

---
+++ operadores.js
@@ -0,0 +1,35 @@
+// Operacores MAtematicos
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 1);
+console.log(1 / 1);
+console.log(1 % 1);
+console.log(3 ** 3);
+
+// MAs operadores 
+let a = 10; //a = a operadores 10
+console.log(a);
+a += 10;
+console.log(a);
+a -= 10;
+console.log(a);
+a *= 10;
+console.log(a);
+a /= 10;
+console.log(a);
+a %= 10;
+console.log(a);
+
+// Operadores relacionales
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 !== 10);
+console.log(10 === 10);
+
+// Operadores logicos
+console.log(true && true);
+console.log(true || true);
+console.log(!true);
