508dc5584168d47018894489e222f70b807e84a6
SuperExe404 <diegomon0440@gmail.com>
2023-09-08 14:37:45 -0600

    02 Variables let y conts

---
+++ variables.js
@@ -0,0 +1,32 @@
+// Js no es tipado por lo que no se especifica tipo de dato
+
+// Para declaracion de variables
+// Utilizamos conts y let
+// conts para variable de solo lectura
+// let para variables 
+
+const pi = 3.14;
+// No podemos asignar nuevos valores a conts
+//pi = "tres";
+
+// Debemos inicualizar siempre conts 
+//const g;
+//g = 1.89;
+
+let contador;
+console.log(contador)
+
+contador = 10;
+contador += 1;
+contador += 1;
+contador += 1;
+contador += 1;
+
+console.log(contador);
+
+contador = "Ahora soy un string";
+console.log(contador);
+
+let verdad = true;
+console.log(typeof(verdad))
+console.log(typeof(contador))
