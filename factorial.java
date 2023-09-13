437f40a8a60eb20648165723a36cc6085c144edf
SuperExe404 <diegomon0440@gmail.com>
2023-09-13 09:27:19 -0600

    06 Funciones

---
+++ factorial.java
@@ -0,0 +1,16 @@
+private int factorial(intnum){
+    if(num==0)
+      return 1;
+    else
+        return num *factorial(num-1)
+}
+
+private static int factorial(int num){
+    int fac = 1;
+    if(num!=0){
+        for(int i = num; 0>0; i--){
+            fact *=i;
+        }
+    }
+    return fact;
+}
---
+++ funciones.js
@@ -0,0 +1,65 @@
+// Funciones en JavaScript
+/*
+Function nombre(params){
+    body function
+}
+const nombre = function (params){
+    body function
+}
+*/
+// Funciones sin parámetros, y sin retorno
+function saludo(){
+    console.log('Helloooo!')
+}
+saludo();
+
+//Funciones con parámetros sin retorno 
+const saludaA = function(name){
+    console.log('Hola ' + name);
+}
+saludaA('Clark Kent');
+
+// Funciones con parámetros y con retorno
+const fact = function (num) {
+    if (num === 0) {
+        return 1;
+    }
+    return fact(num - 1) * num;
+}
+const result = fact(5);
+console.log(result);
+
+// Arrow Functions o funciones de flecha
+// Sin parametros y sin retorno
+const saludaEnChino = () => {
+    console.log('Ni Hao')
+}
+saludaEnChino();
+
+// con 1 parametro sin retorno 
+const mayuscula = (word) =>{
+    console.log(word.toUpperCase());
+}
+mayuscula('hola');
+
+const minuscula = word => {
+    console.log(word.toLowerCase());
+}
+minuscula('HELLO');
+
+// Dos o más parametros y con retorno 
+const fullName = (firstName, lastname) => {
+    return firstName + ' ' + lastname;
+}
+const nombreCompleto = fullName('Bruce','Wayne');
+console.log(nombreCompleto);
+
+// Si el retun es la única línea se puede simplificar 
+const duplica = num => num * 2;
+const doble = duplica(36); 
+console.log(doble);
+
+/* function duplica(num){
+    return num * 2;
+}
+*/
